a974ca314020fef0562e935bbf84c5bcfe0da7c3
Saydur9824 <saydur9827@gmail.com>
2021-11-10 03:29:00 +0600

    ph

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ async function run() {
 
       app.get('/appointments', verifyToken, async(req, res) =>{
         const email = req.query.email;
-        const date =new Date(req.query.date).toLocaleDateString();
-        console.log(date)
+        const date = req.query.date;
+        
         const query = {email:email, date:date}
         console.log(query);
         const cursor = appointmentsCollection.find(query);
