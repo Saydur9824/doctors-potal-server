a90f41e12a98063ceb912595386ddf6b9f7e70ae
Saydur9824 <saydur9827@gmail.com>
2021-11-20 11:45:06 +0600

    ok

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const { query, application } = require('express');
 const stripe = require("stripe")(process.env.STRIPE_SECRET)
-const fileUpload = require('express-fileUpload');
+const fileUpload = require('express-fileupload');
 
 
 const port = process.env.PORT || 5000;
